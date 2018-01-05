68fa00b775599003097c6028d2efe5702678430a
felipecalvo239@gmail.com <felipecalvo239@gmail.com>
2018-01-05 13:26:47 -0500

    Mumuki Export on 2018-01-05 13:26:47 -0500

---
+++ 00007_Objetos bien conocidos/corollary.md
@@ -0,0 +1,13 @@
+:warning: Muchas veces, en lugar de decir que le enviamos un mensaje al objeto apuntado por la referencia `fito`, podemos llegar a decir...
+
+> _enviar un mensaje a la variable_ `fito`
+
+...o...
+
+> _enviar un mensaje al objeto_ `fito`
+
+...o simplemente...
+
+> _enviar un mensaje a_ `fito`
+
+...porque si bien no es del todo correcto, es más breve :sweat_smile:. Lo importante es que entiendas que *siempre* estamos enviando el mensaje al objeto a través de una referencia. 
--- 00007_Objetos bien conocidos/test.rb
+++ 00007_Objetos bien conocidos/test.rb
@@ -1,24 +1,19 @@
-test "existe abuelaClotilde" {
-  abuelaClotilde
-}
+it "objetos_equivalentes es una lista" do
+  expect(objetos_equivalentes.is_a? Array).to be true
+end
 
-test "abuelaClotilde entiende el mensaje nieto(unNieto)" {
-  abuelaClotilde.nieto(fito)
-}
+it "abuela_clotilde no debe inicializar por defecto a su nieto" do
+  expect({abuela_clotilde.alimentar_nieto}).to raise_error
+end
 
-test "abuelaClotilde no debe inicializar por defecto a su nieto" {
-  assert.throwsException({abuelaClotilde.alimentarNieto()})
-}
+it "Cuando abuela_clotilde alimenta a su nieto lo engorda 3kg" do
+  abuela_clotilde.nieto fito
+  abuela_clotilde.alimentar_nieto
+  expect(fito.peso).to eq 73
+end
 
-test "abuelaClotilde.alimentarNieto() incrementa lo engorda 3 kg" {
-  abuelaClotilde.nieto(fito)
-  abuelaClotilde.alimentarNieto()
-  assert.equals(73, fito.peso())
-}
-
-
-test "abuelaClotilde.alimentarNieto() lo alimenta dos veces" {
-  abuelaClotilde.nieto(fito)
-  abuelaClotilde.alimentarNieto()
-  assert.equals(2, fito.cantidadDeLlamadas())
-}
+it "La abuela alimenta a su nieto dos veces" do
+  abuela_clotilde.nieto fito
+  abuela_clotilde.alimentar_nieto
+  expect(fito.cantidad_de_llamadas).to eq 2
+end
