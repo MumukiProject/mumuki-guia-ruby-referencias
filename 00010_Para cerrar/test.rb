82b48984558fa4333ee6d44d62d53adf47af8c6c
felipecalvo239@gmail.com <felipecalvo239@gmail.com>
2018-01-07 18:58:26 -0500

    Mumuki Export on 2018-01-07 18:58:26 -0500

--- 00010_Para cerrar/test.rb
+++ 00010_Para cerrar/test.rb
@@ -3,7 +3,7 @@ it "El PianoFamiliar está inicialmente afinado" do
 end
 
 it "Después de tocar el piano 21 veces, ya no está afinado" do
-  Jazmin.piano(PianoFamiliar)  
+  Jazmin.piano=(PianoFamiliar)  
   21.times { jazmin.tocar }
   expect(PianoFamiliar.esta_afinado?).to be false
 end
@@ -14,21 +14,21 @@ it "Lucio sabe afinar el piano" do
 end
 
 it "Después de tocar el piano 21 veces, y afinarlo durante una hora, está afinado" do
-  Jazmin.piano(PianoFamiliar)  
+  Jazmin.piano=(PianoFamiliar)  
   21.times { jazmin.tocar }
   Lucio.afinar(PianoFamiliar, 1)
   expect(PianoFamiliar.esta_afinado?).to be true
 end
 
 it "Después de tocar el piano 90 veces, y afinarlo durante una hora, no está afinado" do
-  Jazmin.piano(PianoFamiliar)  
+  Jazmin.piano=(PianoFamiliar)  
   90.times { jazmin.tocar }
   Lucio.afinar(PianoFamiliar, 1)
   expect(PianoFamiliar.esta_afinado?).to be false
 end
 
 it "después de tocar el piano 90 veces, y afinarlo durante 20 horas, está afinado" do
-  Jazmin.piano(PianoFamiliar)  
+  Jazmin.piano=(PianoFamiliar)  
   90.times { jazmin.tocar }
   Lucio.afinar(PianoFamiliar, 20)
   expect(PianoFamiliar.esta_afinado?).to be false
@@ -36,7 +36,7 @@ end
 
 it "la afinación máxima es 100 aunque afine el piano durante 743 horas" do
   Lucio.afinar(PianoFamiliar, 743)
-  Jazmin.piano(PianoFamiliar)  
+  Jazmin.piano=(PianoFamiliar)  
   90.times { jazmin.tocar }
   expect(PianoFamiliar.esta_afinado?).to be false
 end
