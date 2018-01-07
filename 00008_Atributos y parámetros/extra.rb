a5e6cbb979afa6d3ed3dd5419ab391ce932afda0
felipecalvo239@gmail.com <felipecalvo239@gmail.com>
2018-01-07 11:03:40 -0500

    Mumuki Export on 2018-01-07 11:03:40 -0500

--- 00008_Atributos y parámetros/extra.rb
+++ 00008_Atributos y parámetros/extra.rb
@@ -1,12 +1,12 @@
 module Pepita
   @ciudad
   
-  def self.ciudad=(una_ciudad)
-    @ciudad = una_ciudad
-  end
-  
   def self.ciudad
     @ciudad
+  end
+  
+  def self.ciudad=(una_ciudad)
+    @ciudad = una_ciudad
   end
 end
 
