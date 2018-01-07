60d9af378033c056d36f05f633590b33e39c5559
felipecalvo239@gmail.com <felipecalvo239@gmail.com>
2018-01-07 10:24:17 -0500

    Mumuki Export on 2018-01-07 10:24:17 -0500

--- 00007_Objetos bien conocidos/test.rb
+++ 00007_Objetos bien conocidos/test.rb
@@ -1,19 +1,15 @@
-it "objetos_equivalentes es una lista" do
-  expect(objetos_equivalentes.is_a? Array).to be true
-end
-
 it "AbuelaClotilde no debe inicializar por defecto a su nieto" do
   expect { AbuelaClotilde.alimentar_nieto }.to raise_error
 end
 
-it "Cuando AbuelaClotilde alimenta a su nieto lo engorda 3kg" do
-  AbuelaClotilde.nieto Fito
-  AbuelaClotilde.alimentar_nieto
-  expect(Fito.peso).to eq 73
-end
+#it "Cuando AbuelaClotilde alimenta a su nieto lo engorda 3kg" do
+#  AbuelaClotilde.nieto Fito
+#  AbuelaClotilde.alimentar_nieto
+#  expect(Fito.peso).to eq 73
+#end
 
-it "AbuelaClotilde alimenta a su nieto dos veces" do
-  AbuelaClotilde.nieto Fito
-  AbuelaClotilde.alimentar_nieto
-  expect(Fito.cantidad_de_llamadas).to eq 2
-end
+#it "AbuelaClotilde alimenta a su nieto dos veces" do
+#  AbuelaClotilde.nieto Fito
+#  AbuelaClotilde.alimentar_nieto
+#  expect(Fito.cantidad_de_llamadas).to eq 2
+#end
