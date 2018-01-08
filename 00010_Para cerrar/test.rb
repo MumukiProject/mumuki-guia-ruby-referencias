d860cba0185a1127ec76898eccdc1b4c7b46e759
felipecalvo239@gmail.com <felipecalvo239@gmail.com>
2018-01-07 19:15:50 -0500

    Mumuki Export on 2018-01-07 19:15:50 -0500

--- 00010_Para cerrar/test.rb
+++ 00010_Para cerrar/test.rb
@@ -11,17 +11,17 @@ it "Inicialmente Jazmín no tiene un piano" do
   expect { Jazmin.tocar }.to raise_error
 end
 
-it "Después de tocar el piano 23 veces, ya no está afinado" do
-  Jazmin.piano=(PianoFamiliar)  
-  23.times { Jazmin.tocar }
-  expect(PianoFamiliar.esta_afinado?).to be false
-end
-
 it "Después de tocar el piano 23 veces, y afinarlo durante una hora, está afinado" do
   Jazmin.piano=(PianoFamiliar)  
   23.times { Jazmin.tocar }
   Lucio.afinar(PianoFamiliar, 1)
   expect(PianoFamiliar.esta_afinado?).to be true
+end
+
+it "Después de tocar el piano 23 veces, ya no está afinado" do
+  Jazmin.piano=(PianoFamiliar)  
+  23.times { Jazmin.tocar }
+  expect(PianoFamiliar.esta_afinado?).to be false
 end
 
 it "Después de tocar el piano 90 veces, y afinarlo durante una hora, no está afinado" do
