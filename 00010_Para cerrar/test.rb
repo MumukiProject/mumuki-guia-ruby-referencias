f0064b49bb1e8a66240a9f08fcb7d9d0d3372954
gustavo@mumuki.org <gustavo@mumuki.org>
2021-02-24 22:06:55 +0000

    Mumuki Export on 2021-02-24 22:06:55 +0000

--- 00010_Para cerrar/test.rb
+++ 00010_Para cerrar/test.rb
@@ -1,50 +1,70 @@
 before do
   Fideos.descartar_la_salsa!
-end
-
-it "Inicialmente Jor no tiene un plato del dia" do
-  expect { Jor.picantear! }.to raise_error
+  Jor.plato_del_dia = Fideos
 end
 
 it "Los Fideos inicialmente no están picantes" do
-  expect(Fideos.picantes?).to be false
+  resultado = Fideos.picantes?
+  expect(resultado).to be(false), "Fideos.picantes? debería ser false porque inicialmente no tienen ajíes. Sin embargo, Fideo.picantes? fue #{resultado}"
 end
 
-it "Cuando Jor picantea su plato pasa a estar picante" do
-  Jor.plato_del_dia = Fideos
+it "Si Jor picantea su plato, los Fideos quedan picantes" do
   Jor.picantear!
-  expect(Fideos.picantes?).to be true
+  resultado = Fideos.picantes?
+  expect(resultado).to be(true), "Fideos.picantes? debería ser true porque deberían tener 5 ajíes. Sin embargo, Fideos.picantes? fue #{resultado}."
 end
 
-it "Los Fideos están picantes cuando Luchi los suaviza quitándole 2 ajíes después de que Jor los picanteó" do
-  Jor.plato_del_dia = Fideos
+it "Si Jor picantea su plato y Luchi luego suaviza en 2 ajíes, los Fideos quedan picantes" do
   Jor.picantear!
   Luchi.suavizar! Fideos, 2
-  expect(Fideos.picantes?).to be true
+  resultado = Fideos.picantes?
+  expect(resultado).to be(true), "Fideos.picantes? debería ser true porque deberían tener 3 ajíes. Sin embargo, Fideos.picantes? fue #{resultado}."
 end
 
-it "Los Fideos dejan de estar picantes cuando Luchi los suaviza quitándoles 3 ajíes después de que Jor los picanteó" do
+begin
+  Fideos.descartar_la_salsa!
   Jor.plato_del_dia = Fideos
   Jor.picantear!
   Luchi.suavizar! Fideos, 3
-  expect(Fideos.picantes?).to be false
-end
-
-it "Los Fideos están picantes cuando Luchi los suaviza quitándoles 2 ajíes después de que Jor los picanteó 2 veces" do
-  Jor.plato_del_dia = Fideos
-  Jor.picantear!
-  Jor.picantear!
-  Luchi.suavizar! Fideos, 2
   resultado = Fideos.picantes?
-  expect(resultado).to be(true), "Fideos.picantes? debería ser true, pero fue #{resultado}"
+  it "Si Jor picantea su plato y Luchi luego suaviza en 3 ajíes, los Fideos no quedan picantes" do
+    expect(resultado).to be(false), "Fideos.picantes? debería ser false porque deberían tener 2 ajíes. Sin embargo, Fideos.picantes? fue #{resultado}."
+  end
+rescue => e
 end
 
-it "Los Fideos no están picantes cuando Luchi los suaviza quitándoles 4 ajíes después de que Jor los picanteó 3 veces porque se descarta la salsa" do
-  Jor.plato_del_dia = Fideos
+it "Si Jor picantea su plato dos veces y Luchi luego suaviza en 1 ají, los Fideos quedan picantes" do
   Jor.picantear!
   Jor.picantear!
+  Luchi.suavizar! Fideos, 1
+  resultado = Fideos.picantes?
+  expect(resultado).to be(true), "Fideos.picantes? debería ser true porque deberían tener 9 ajíes. Sin embargo, Fideos.picantes? fue #{resultado}."
+end
+
+it "Luchi descarta la salsa cuando tiene más de 10 ajíes" do
   Jor.picantear!
   Luchi.suavizar! Fideos, 4
-  resultado = Fideos.picantes?
-  expect(resultado).to be(false), "Fideos.picantes? debería ser false, pero fue #{resultado}"
+  Jor.picantear!
+  Jor.picantear!
+  
+  allow(Fideos).to receive(:descartar_la_salsa!)
+  Luchi.suavizar! Fideos, 1
+  expect(Fideos).to have_received(:descartar_la_salsa!), "Luchi debería descartar la salsa porque Fideos debería tener 11 ajíes"
+end
+  
+it "Luchi no descarta la salsa cuando tiene 10 ajíes" do
+  Jor.picantear!
+  Jor.picantear!
+  allow(Fideos).to receive(:descartar_la_salsa!)
+  Luchi.suavizar! Fideos, 4
+  expect(Fideos).not_to have_received(:descartar_la_salsa!), "Luchi no debería descartar la salsa porque Fideos debería tener 10 ajíes"
+end
+
+it "Luchi no descarta la salsa cuando tiene menos de 10 ajíes" do
+  Jor.picantear!
+  Luchi.suavizar! Fideos, 1
+  Jor.picantear!
+  allow(Fideos).to receive(:descartar_la_salsa!)
+  Luchi.suavizar! Fideos, 1
+  expect(Fideos).not_to have_received(:descartar_la_salsa!), "Luchi no debería descartar la salsa porque Fideos debería tener 5 ajíes"
 end
